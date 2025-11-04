1082b8d006502cff608a0cc7cc07c96c5f31491a
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-11-04 15:19:02 +0100

    Add printf debug

--- test_mm.c
+++ test_mm.c
@@ -16,10 +16,16 @@ int main(int argc, char ** argv) {
 
   /* Ensure that macros are working */
   int ret = simple_macro_test();
+  printf(ret == 0 ? "SUCCESS\n" : "FAILURE\n");
+  printf("%d\n", ret);
   if (ret > 0) {
     printf("Macro test returned %d\n", ret);
     return 1;
   }
+
+    printf("Start simple test\n");
+
+
 
   void * a = simple_malloc(0x200);
 
